54e2d52f33cfba22b0292e8c55942398ae5d53a9
MinJun <overyamu@gmail.com>
2022-05-06 18:05:06 +0900

    test

--- ProjectRoR2/Assets/Minjun/Script/ItemBox.cs
+++ ProjectRoR2/Assets/Minjun/Script/ItemBox.cs
@@ -4,28 +4,51 @@ using UnityEngine;
 
 public class ItemBox : MonoBehaviour
 {
-    public Transform myRoot;
+    bool BoxEngage = false;
+    bool BoxEmpty = false;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        //float RespawnPos_x = Random.Range(-10, 10);
-        //float RespawnPos_z = Random.Range(-10, 10);
-        //myRoot.transform.position = new Vector3(RespawnPos_x, 0.0f, RespawnPos_z);
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (BoxEngage && !BoxEmpty)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.enabled = true;
+                GameObject respawn = Instantiate(Resources.Load("Effect/RespawnLight1"), this.transform.localPosition, Quaternion.Euler(new Vector3(-90.0f,0f,0f))) as GameObject;
+                StartCoroutine(ItemRespawn());
+                
+                BoxEmpty = true;
+                BoxCollider Box = GetComponent<BoxCollider>();
+                Destroy(Box);
+            }
+        }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        BoxEngage = true;
+    }
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            GameObject obj = Instantiate(Resources.Load("Item/CrowBar"),this.transform.localPosition + new Vector3(0f,2f,0f), Quaternion.identity) as GameObject;
-            obj.transform.localScale += new Vector3(50f, 50f, 50f);
-            Destroy(this.transform.parent.gameObject);
-        }
+        BoxEngage = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        BoxEngage = false;
+    }
+
+    IEnumerator ItemRespawn()
+    {
+        yield return new WaitForSeconds(3.0f);
+        GameObject obj = Instantiate(Resources.Load("Item/CrowBar"), this.transform.localPosition + new Vector3(0f, 2f, 0f), Quaternion.identity) as GameObject;
+        obj.transform.localScale += new Vector3(50f, 50f, 50f);
     }
 }
--- ProjectRoR2/Assets/Minjun/Script/Player.cs
+++ ProjectRoR2/Assets/Minjun/Script/Player.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using System;
 
 public class Player : MonoBehaviour
-{    
+{
     public float MoveSpeed=10.0f;
     public float Hp = 100.0f;
     public float Attack=10.0f;
     public float depense=10.0f;
     public float AttackSpeed=1.0f;
-    
+    public int JumpCount = 1;
+    public float critical = 0;
+    public float HpRecovery = 10f;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKey(KeyCode.A))
         {
             this.transform.Translate(Vector3.left * MoveSpeed * Time.deltaTime);
