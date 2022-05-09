9f5823f4bb040f46ba09acaf61b2c74c8d228db6
MinJun <overyamu@gmail.com>
2022-05-09 20:18:54 +0900

    0509

--- ProjectRoR2/Assets/Minjun/Script/Item.cs
+++ ProjectRoR2/Assets/Minjun/Script/Item.cs
@@ -12,16 +12,10 @@ public class Item : MonoBehaviour
     float SyringeClusterSpeed;
     void Start()
     {
-<<<<<<< Updated upstream
-        playerStat = GameObject.Find("mdlCommandoDualies").GetComponent<Player>();
-        SyringeClusterSpeed = playerStat.AttackSpeed * 0.15f;
-        HoofSpeed = playerStat.MoveSpeed * 0.14f;
-=======
         playerStat = GameObject.Find("mdlCommandoDualies").GetComponent<Player>(); //스탯을 받아주기 위해 
         SyringeClusterSpeed = playerStat.myCharacterStat.AttackSpeed * 0.15f;
         HoofWalkSpeed = playerStat.myCharacterStat.WalkSpeed * 0.14f;
         HoofRunSpeed = playerStat.myCharacterStat.RunSpeed * 0.14f;
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -35,101 +29,6 @@ public class Item : MonoBehaviour
         //1티어
         if (this.gameObject.name.Contains("Bullet"))
         {
-<<<<<<< Updated upstream
-            if (playerStat.eqiup[0] == 0)
-            {
-                playerStat.eqiup[0]++;
-            }
-            else
-            {
-                playerStat.eqiup[0]++;
-                Destroy(this.gameObject);
-            }
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 허벅지)
-        }
-        if (this.gameObject.name.Contains("Crowbar"))
-        {
-            if (playerStat.eqiup[1] == 0)
-            {
-                playerStat.eqiup[1]++;
-            }
-            else
-            {
-                playerStat.eqiup[1]++;
-                Destroy(this.gameObject);
-            }
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (등 오른쪽)
-        }
-        if (this.gameObject.name.Contains("Glasses"))
-        {
-            if (playerStat.eqiup[2] == 0)
-            {
-                playerStat.eqiup[2]++;
-            }
-            else
-            {
-                playerStat.eqiup[2]++;
-                Destroy(this.gameObject);
-            }
-            playerStat.critical += 0.1f;
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (얼굴)
-        }
-        if (this.gameObject.name.Contains("Goat_Hoof")) 
-        {
-            playerStat.MoveSpeed += HoofSpeed;
-            if (playerStat.eqiup[3] == 0)
-            {
-                playerStat.eqiup[3]++;
-            }
-            else
-            {
-                playerStat.eqiup[3]++;
-                Destroy(this.gameObject);
-            }
-            
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 발)
-        }
-        if (this.gameObject.name.Contains("Medkit"))
-        {
-            if (playerStat.eqiup[4] == 0)
-            {
-                playerStat.eqiup[4]++;
-            }
-            else
-            {
-                playerStat.eqiup[4]++;
-                Destroy(this.gameObject);
-            }
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (허리 오른쪽)
-        }
-        if (this.gameObject.name.Contains("Steak")) 
-        {
-            if (playerStat.eqiup[5] == 0)
-            {
-                playerStat.eqiup[5]++;
-            }
-            else
-            {
-                playerStat.eqiup[5]++;
-                Destroy(this.gameObject);
-            }
-            playerStat.Hp += 25.0f;
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (가슴)
-        }
-        if (this.gameObject.name.Contains("SyringeCluster"))
-        {
-            if (playerStat.eqiup[6] == 0)
-            {
-                playerStat.eqiup[6]++;
-            }
-            else
-            {
-                playerStat.eqiup[6]++;
-                Destroy(this.gameObject);
-            }
-            playerStat.AttackSpeed += SyringeClusterSpeed;
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기(오른쪽 허벅지)
-=======
             //보스에게 20%(중첩당 +20%)의 피해를 추가로 입힙니다.[합연산]
             if (playerStat.equip[0] == 0)
             {
@@ -236,78 +135,11 @@ public class Item : MonoBehaviour
                 playerStat.equip[6]++;
                 Destroy(this.gameObject);
             }
->>>>>>> Stashed changes
         }
 
         //2티어
         if (this.gameObject.name.Contains("Guillotine"))
         {
-<<<<<<< Updated upstream
-            if (playerStat.eqiup[7] == 0)
-            {
-                playerStat.eqiup[7]++;
-            }
-            else
-            {
-                playerStat.eqiup[7]++;
-                Destroy(this.gameObject);
-            }
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기  (등 가운데)
-        }
-        if (this.gameObject.name.Contains("Hopoo_Feather"))
-        {
-            playerStat.JumpCount += 1;
-            if (playerStat.eqiup[8] == 0)
-            {
-                playerStat.eqiup[8]++;
-            }
-            else
-            {
-                playerStat.eqiup[8]++;
-                Destroy(this.gameObject);
-            }
-            
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (왼쪽 팔)
-        }
-        if (this.gameObject.name.Contains("MissileLauncher"))
-        {
-            if (playerStat.eqiup[9] == 0)
-            {
-                playerStat.eqiup[9]++;
-            }
-            else
-            {
-                playerStat.eqiup[9]++;
-                Destroy(this.gameObject);
-            }
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (왼쪽 어꺠)
-        }
-        if (this.gameObject.name.Contains("Seed"))
-        {
-            if (playerStat.eqiup[10] == 0)
-            {
-                playerStat.eqiup[10]++;
-            }
-            else
-            {
-                playerStat.eqiup[10]++;
-                Destroy(this.gameObject);
-            }
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (왼쪽 허리)
-        }
-        if (this.gameObject.name.Contains("Time_Bubble"))
-        {
-            if (playerStat.eqiup[11] == 0)
-            {
-                playerStat.eqiup[11]++;
-            }
-            else
-            {
-                playerStat.eqiup[11]++;
-                Destroy(this.gameObject);
-            }
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 허벅지)
-=======
             //체력이 13%(중첩당 +13%) 이하인 엘리트 몬스터들을 즉시 처치합니다.[곱연산][ 계산식 : 1 - 1 / (1 + 0.13 * 단두대 개수) ]
 
             if (playerStat.equip[7] == 0)
@@ -381,7 +213,6 @@ public class Item : MonoBehaviour
                 playerStat.equip[11]++;
                 Destroy(this.gameObject);
             }
->>>>>>> Stashed changes
         }
 
         //보스유물
@@ -391,15 +222,9 @@ public class Item : MonoBehaviour
             playerStat.myCharacterStat.Hp_Heal += 1.6f;
             //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 어깨)
             GameObject Head = GameObject.Find("head");
-<<<<<<< Updated upstream
-            if(playerStat.eqiup[12] == 0) 
-            {
-                playerStat.eqiup[12]++;
-=======
             if(playerStat.equip[12]== 0) 
             {
                 playerStat.equip[12]++;
->>>>>>> Stashed changes
                 this.transform.parent = Head.transform;
                 this.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
                 this.gameObject.transform.position = Head.transform.position + new Vector3(0.145f, 0f, 0f);
@@ -409,28 +234,12 @@ public class Item : MonoBehaviour
             }
             else
             {
-<<<<<<< Updated upstream
-                playerStat.eqiup[12]++;
-=======
                 playerStat.equip[12]++;
->>>>>>> Stashed changes
                 Destroy(this.gameObject);
             }
         }
         if (this.gameObject.name.Contains("LightningStrike"))
         {
-<<<<<<< Updated upstream
-            if(playerStat.eqiup[13] == 0)
-            {
-                playerStat.eqiup[13]++;
-            }
-            else
-            {
-                playerStat.eqiup[13]++;
-                Destroy(this.gameObject);
-            }
-            //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기(왼쪽 어깨)
-=======
             //공격이 명중하면 10% 확률로 벼락이 쳐서 500%(중첩당 +500%)피해를 입힙니다.[합연산]
             if (playerStat.equip[13] == 0)
             {
@@ -442,7 +251,6 @@ public class Item : MonoBehaviour
                 playerStat.equip[13]++;
                 Destroy(this.gameObject);
             }
->>>>>>> Stashed changes
         }
 
     }
