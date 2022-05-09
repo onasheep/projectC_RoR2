a18036fa6b0b9d9773c10ab758cb339329caf3f3
MinJun <overyamu@gmail.com>
2022-05-09 19:52:16 +0900

    0509

--- ProjectRoR2/Assets/Minjun/Script/Item.cs
+++ ProjectRoR2/Assets/Minjun/Script/Item.cs
@@ -7,13 +7,21 @@ public class Item : MonoBehaviour
     public GameObject myPlayer;
     public LayerMask playerLayer;
     Player playerStat;
-    float HoofSpeed;
+    float HoofWalkSpeed;
+    float HoofRunSpeed;
     float SyringeClusterSpeed;
     void Start()
     {
+<<<<<<< Updated upstream
         playerStat = GameObject.Find("mdlCommandoDualies").GetComponent<Player>();
         SyringeClusterSpeed = playerStat.AttackSpeed * 0.15f;
         HoofSpeed = playerStat.MoveSpeed * 0.14f;
+=======
+        playerStat = GameObject.Find("mdlCommandoDualies").GetComponent<Player>(); //스탯을 받아주기 위해 
+        SyringeClusterSpeed = playerStat.myCharacterStat.AttackSpeed * 0.15f;
+        HoofWalkSpeed = playerStat.myCharacterStat.WalkSpeed * 0.14f;
+        HoofRunSpeed = playerStat.myCharacterStat.RunSpeed * 0.14f;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -27,6 +35,7 @@ public class Item : MonoBehaviour
         //1티어
         if (this.gameObject.name.Contains("Bullet"))
         {
+<<<<<<< Updated upstream
             if (playerStat.eqiup[0] == 0)
             {
                 playerStat.eqiup[0]++;
@@ -120,11 +129,120 @@ public class Item : MonoBehaviour
             }
             playerStat.AttackSpeed += SyringeClusterSpeed;
             //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기(오른쪽 허벅지)
+=======
+            //보스에게 20%(중첩당 +20%)의 피해를 추가로 입힙니다.[합연산]
+            if (playerStat.equip[0] == 0)
+            {
+                playerStat.equip[0]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 허벅지)
+            }
+            else
+            {
+                playerStat.equip[0]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("Crowbar"))
+        {
+            //체력이 90%를 넘는 적에게 75%(중첩당 +75%)의 피해를 입힙니다.[합연산]
+
+            if (playerStat.equip[1] == 0)
+            {
+                playerStat.equip[1]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (등 오른쪽)
+            }
+            else
+            {
+                playerStat.equip[1]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("Glasses"))
+        {
+            //공격 시에 10 % (중첩당 + 10 %) 확률로 '치명타'가 발생하여, 두 배의 피해를 입힙니다.[합연산]
+            //playerStat.myCharacterStat.critical += 0.1f;  크리티컬 미구현
+            
+            if (playerStat.equip[2] == 0)
+            {
+                playerStat.equip[2]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (얼굴)
+            }
+            else
+            {
+                playerStat.equip[2]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("Goat_Hoof")) 
+        {
+            //이동 속도가 14%(중첩당 +14%) 증가합니다.[합연산]
+            playerStat.myCharacterStat.WalkSpeed += HoofWalkSpeed;
+            playerStat.myCharacterStat.RunSpeed += HoofRunSpeed;
+
+            if (playerStat.equip[3] == 0)
+            {
+                playerStat.equip[3]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 발)
+            }
+            else
+            {
+                playerStat.equip[3]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("Medkit"))
+        {
+            //피해를 받으면 2초 후 체력이 20 치유되고, 추가로 최대 체력의 5%(중첩당 +5%)가 치유됩니다.[합연산]
+
+            if (playerStat.equip[4] == 0)
+            {
+                playerStat.equip[4]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (허리 오른쪽)
+            }
+            else
+            {
+                playerStat.equip[4]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("Steak")) 
+        {
+            //최대 체력이 25(중첩당 + 25) 증가합니다.[합연산]
+            playerStat.myCharacterStat.Hp += 25.0f;
+
+            if (playerStat.equip[5] == 0)
+            {
+                playerStat.equip[5]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (가슴)
+            }
+            else
+            {
+                playerStat.equip[5]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("SyringeCluster"))
+        {
+            //공격 속도가 15 % (중첩당 + 15 %) 증가합니다.[합연산]
+            playerStat.myCharacterStat.AttackSpeed += SyringeClusterSpeed;
+
+            if (playerStat.equip[6] == 0)
+            {
+                playerStat.equip[6]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기(오른쪽 허벅지)
+            }
+            else
+            {
+                playerStat.equip[6]++;
+                Destroy(this.gameObject);
+            }
+>>>>>>> Stashed changes
         }
 
         //2티어
         if (this.gameObject.name.Contains("Guillotine"))
         {
+<<<<<<< Updated upstream
             if (playerStat.eqiup[7] == 0)
             {
                 playerStat.eqiup[7]++;
@@ -189,19 +307,99 @@ public class Item : MonoBehaviour
                 Destroy(this.gameObject);
             }
             //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 허벅지)
+=======
+            //체력이 13%(중첩당 +13%) 이하인 엘리트 몬스터들을 즉시 처치합니다.[곱연산][ 계산식 : 1 - 1 / (1 + 0.13 * 단두대 개수) ]
+
+            if (playerStat.equip[7] == 0)
+            {
+                playerStat.equip[7]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기  (등 가운데)
+            }
+            else
+            {
+                playerStat.equip[7]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("Hopoo_Feather"))
+        {
+            //최대 점프 횟수가 1(중첩당 +1)회 증가합니다.[합연산]
+            playerStat.myCharacterStat.JumpCount += 1;
+
+            if (playerStat.equip[8] == 0)
+            {
+                playerStat.equip[8]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (왼쪽 팔)
+            }
+            else
+            {
+                playerStat.equip[8]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("MissileLauncher"))
+        {
+            //10% 확률로 미사일을 발사하여 총 300%(중첩당 +300%)의 피해를 입힙니다.[합연산]
+
+            if (playerStat.equip[9] == 0)
+            {
+                playerStat.equip[9]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (왼쪽 어꺠)
+            }
+            else
+            {
+                playerStat.equip[9]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("Seed"))
+        {
+            //피해를 입히면 1(중첩당 +1)의 체력이 치유됩니다.[합연산]
+
+            if (playerStat.equip[10] == 0)
+            {
+                playerStat.equip[10]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (왼쪽 허리)
+            }
+            else
+            {
+                playerStat.equip[10]++;
+                Destroy(this.gameObject);
+            }
+        }
+        if (this.gameObject.name.Contains("Time_Bubble"))
+        {
+            //공격 명중 시 그 적을 느리게 하여, 2초(중첩당 +2초) 동안 이동 속도를 60% 감소시킵니다.[합연산]
+
+            if (playerStat.equip[11] == 0)
+            {
+                playerStat.equip[11]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 허벅지)
+            }
+            else
+            {
+                playerStat.equip[11]++;
+                Destroy(this.gameObject);
+            }
+>>>>>>> Stashed changes
         }
 
         //보스유물
         if (this.gameObject.name.Contains("Titan_Knurl"))
         {
-            int equip = 0;
-            playerStat.Hp += 40.0f;
-            playerStat.HpRecovery += 1.6f;
+            playerStat.myCharacterStat.Hp += 40.0f;
+            playerStat.myCharacterStat.Hp_Heal += 1.6f;
             //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기 (오른쪽 어깨)
             GameObject Head = GameObject.Find("head");
+<<<<<<< Updated upstream
             if(playerStat.eqiup[12] == 0) 
             {
                 playerStat.eqiup[12]++;
+=======
+            if(playerStat.equip[12]== 0) 
+            {
+                playerStat.equip[12]++;
+>>>>>>> Stashed changes
                 this.transform.parent = Head.transform;
                 this.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
                 this.gameObject.transform.position = Head.transform.position + new Vector3(0.145f, 0f, 0f);
@@ -211,12 +409,17 @@ public class Item : MonoBehaviour
             }
             else
             {
+<<<<<<< Updated upstream
                 playerStat.eqiup[12]++;
+=======
+                playerStat.equip[12]++;
+>>>>>>> Stashed changes
                 Destroy(this.gameObject);
             }
         }
         if (this.gameObject.name.Contains("LightningStrike"))
         {
+<<<<<<< Updated upstream
             if(playerStat.eqiup[13] == 0)
             {
                 playerStat.eqiup[13]++;
@@ -227,6 +430,19 @@ public class Item : MonoBehaviour
                 Destroy(this.gameObject);
             }
             //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기(왼쪽 어깨)
+=======
+            //공격이 명중하면 10% 확률로 벼락이 쳐서 500%(중첩당 +500%)피해를 입힙니다.[합연산]
+            if (playerStat.equip[13] == 0)
+            {
+                playerStat.equip[13]++;
+                //아이템 먹으면 플레이어에게 붙게끔 위치 잡아주기(왼쪽 어깨)
+            }
+            else
+            {
+                playerStat.equip[13]++;
+                Destroy(this.gameObject);
+            }
+>>>>>>> Stashed changes
         }
 
     }
