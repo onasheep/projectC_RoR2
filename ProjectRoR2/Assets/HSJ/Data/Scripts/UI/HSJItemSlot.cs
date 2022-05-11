07457875e57f1364a7bb0d867c9ca8fc91dcd3a5
onasheep <inabass@naver.com>
2022-05-11 21:54:25 +0900

    인벤토리 업데이트
    UI 배치 / HSJ로 아이템, 아이템슬롯, 인벤토리 스크립트 생성

---
+++ ProjectRoR2/Assets/HSJ/Data/Scripts/UI/HSJItemSlot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HSJItemSlot : MonoBehaviour
+{
+    public int i;
+    private HsjInventory inventory;
+
+    private void Awake()
+    {
+        inventory = GameObject.FindObjectOfType<HsjInventory>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.childCount <= 0)
+        {
+            inventory.fullCheck[i] = false;
+        }
+    }
+}
---
+++ ProjectRoR2/Assets/HSJ/Data/Scripts/UI/HsjInventory.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HsjInventory : MonoBehaviour
+{
+    public Sprite DefaulImage;
+    public int MaxCount;
+    public bool[] fullCheck;
+    public GameObject[] slots;
+
+    private HsjInventory Iv;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
