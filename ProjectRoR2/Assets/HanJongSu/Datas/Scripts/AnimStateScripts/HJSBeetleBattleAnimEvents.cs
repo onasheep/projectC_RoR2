b393e11d86a9e07c4013bbccb83fe0a75e4a7f97
GoldenCreature <jongsu3908@naver.com>
2022-06-15 22:39:54 +0900

    finalTest
    06월10일

--- ProjectRoR2/Assets/HanJongSu/Datas/Scripts/AnimStateScripts/HJSBeetleBattleAnimEvents.cs
+++ ProjectRoR2/Assets/HanJongSu/Datas/Scripts/AnimStateScripts/HJSBeetleBattleAnimEvents.cs
@@ -5,14 +5,10 @@ using UnityEngine.Events;
 
 public class HJSBeetleBattleAnimEvents : MonoBehaviour
 {
-
-    //public event UnityAction Attack = null;
     public Transform Head;
-    /*public void OnAttack()
-    {
-        Debug.Log("BattleEventsOnAttack");
-        Attack?.Invoke();
-    }*/
+    public GameObject CrashEffect;
+    public AudioClip CrashSound;
+
     public void HeadButColider()
     {
         Collider[] Enemy = Physics.OverlapSphere(Head.position, 3.0f, 1 << LayerMask.NameToLayer("Player"));
@@ -20,7 +16,17 @@ public class HJSBeetleBattleAnimEvents : MonoBehaviour
         {
             foreach (Collider enm in Enemy)
             {
-                enm.GetComponent<KJH_Player>()?.TakeDamage(this.GetComponentInParent<Beetle>().BettleData.AD);
+                //enm.GetComponent<KJH_Player>()?.TakeDamage(this.GetComponentInParent<Beetle>().BettleData.AD);
+                if (DontDestroyobject.instance.CharSelected == 1)
+                {
+                    enm.GetComponent<KJH_Player>()?.TakeDamage(this.GetComponentInParent<Beetle>().BettleData.AD);
+                    GetComponentInParent<Beetle>().SoundPlay(CrashSound);
+                    Instantiate(CrashEffect, Head.position + new Vector3(0.0f, -1.0f, 0.0f), Quaternion.identity);
+                }
+                if (DontDestroyobject.instance.CharSelected == 2)
+                {
+                    //nm.GetComponent<Loader>()?.TakeDamage(this.GetComponentInParent<Beetle>().BettleData.AD);
+                }
             }
         }
 
--- ProjectRoR2/Assets/HanJongSu/Datas/Scripts/Parent/Parent.cs
+++ ProjectRoR2/Assets/HanJongSu/Datas/Scripts/Parent/Parent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Parent : HJSMonster, HJSCombatSystem
 {
@@ -16,20 +17,29 @@ public class Parent : HJSMonster, HJSCombatSystem
         get => Target;
         set => Target = value;
     }
+    float MoveDelay = 0.0f;
     public Transform Hand;
-    float IsAir = 0.0f;
-    float MoveDelay = 10.0f;
+    private AudioSource audioSource;
+    SphereCollider Dr = null;
+    public GameObject DrObject;
+    UnityAction DieAction = null;
+
     public HJSMonsterData ParentData;
 
-    public void HJSGetDamage(float Damage)
+    public void HJSGetDamage(float Damage, UnityAction dieAction = null)
     {
         if (myState == STATE.DIE)
         {
             Debug.Log("이미 죽어 리턴");
             return;
         }
+        if(DieAction == null)
+        {
+
+        }
         ParentData.HP -= Damage;
-        Debug.Log("딱정벌래가 " + Damage + "만큼의 데미지를 입어 현재 체력은 " + ParentData.HP);
+        Instantiate(GetDamageEffect, this.transform.position, Quaternion.identity, this.transform);
+        Debug.Log("부모몬스터가 " + Damage + "만큼의 데미지를 입어 현재 체력은 " + ParentData.HP);
         if (ParentData.HP <= 0)
         {
             ParentData.HP = 0;
@@ -44,8 +54,29 @@ public class Parent : HJSMonster, HJSCombatSystem
         return myState != STATE.DIE;
     }
 
+
+    void ColliderSizeUp(float size, bool wait = false)
+    {
+        Dr = this.GetComponentInChildren<SphereCollider>();
+        Dr.isTrigger = true;
+        if (wait) StartCoroutine(WaitingSecP(size));
+        else Dr.radius = size;  
+        /*
+        if (wait) StartCoroutine(WaitingSecP(size));
+        else DrObject.GetComponent<SphereCollider>().radius = size; 
+        */
+    }
+
+    public void SoundPlay(AudioClip clip)
+    {
+        audioSource.volume = 1.0f;
+        audioSource.PlayOneShot(clip);
+    }
+
     void Start()
     {
+        //ColliderSizeUp(0.1f);
+        audioSource = GetComponent<AudioSource>();
         ChangeState(STATE.CREATE);
     }
 
@@ -55,34 +86,26 @@ public class Parent : HJSMonster, HJSCombatSystem
         ProcessState();
     }
 
-    /*protected void FixedUpdate()
-    {
-        if (Mathf.Approximately(myRigidBody.velocity.y, 0.0f))
-        {
-            if (IsAir != 0.0f)
-            {
-                IsAir = 0.0f;
-            }
-        }
-        else
-        {
-            IsAir += Time.fixedDeltaTime;
-            if (IsAir >= 10.0f)
-            {
-                Destroy(this.gameObject);
-            }
-        }
-    }*/
 
     IEnumerator Slap()
     {
-        if(Target != null) myAnim.SetTrigger("Attack");
+        if (Target != null)
+        {
+            myAnim.SetTrigger("Attack");
+            SoundPlay(AtkSound);
+        }
 
         yield return new WaitForSeconds(ParentData.AttackSpeed);
         ChangeState(STATE.MOVE);
     }
 
-    IEnumerator WaitingSec()
+    IEnumerator WaitingSecP(float size)
+    {
+        yield return new WaitForSeconds(4.0f);
+        DrObject.GetComponent<SphereCollider>().radius = size;
+    }
+
+    IEnumerator WaitingSecForBattle()
     {
         while(!myTarget)
         {
@@ -90,7 +113,7 @@ public class Parent : HJSMonster, HJSCombatSystem
         }
         ChangeState(STATE.BATTLE);
     }
-
+    
     void ChangeState(STATE s)
     {
         if (myState == s) return;
@@ -98,14 +121,18 @@ public class Parent : HJSMonster, HJSCombatSystem
         switch (myState)
         {
             case STATE.CREATE:
+                SoundPlay(SpawnSound);
                 ParentData.HP = 585;
                 ParentData.MaxHP = ParentData.HP;
                 ParentData.AD = 16.0f;
                 ParentData.MoveSpeed = 6.0f;
                 ParentData.AttackRange = 5.0f;
                 ParentData.AttackSpeed = 3.0f;
+                
+                
                 break;
             case STATE.MOVE:
+                
                 if (myTarget)
                 {
                     base.StartMoveTr(myTarget, ParentData.MoveSpeed, ParentData.AttackRange, () => ChangeState(STATE.BATTLE));
@@ -122,11 +149,10 @@ public class Parent : HJSMonster, HJSCombatSystem
                 
                 StopAllCoroutines();
                 StartCoroutine(Slap());
-              
-
                 break;
             case STATE.DIE:
                 StopAllCoroutines();
+                SoundPlay(DieSound);
                 myAnim.SetTrigger("Die");
                 StartCoroutine(Disapearing());
                 break;
@@ -141,6 +167,8 @@ public class Parent : HJSMonster, HJSCombatSystem
             case STATE.CREATE:
                 if (myAnim.GetBool("IsCreate") && myState != STATE.MOVE)
                 {
+                    DrObject.SetActive(true);
+                    //ColliderSizeUp(28.0f);
                     ChangeState(STATE.MOVE);
                 }
                 break;
@@ -156,7 +184,7 @@ public class Parent : HJSMonster, HJSCombatSystem
                 }
                 else if(!myTarget)
                 {
-                    StartCoroutine(WaitingSec());  
+                    StartCoroutine(WaitingSecForBattle());  
                 }
                 
                 break;
